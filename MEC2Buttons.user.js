1bfa75aa55b7ec14f1040b4005474b5ae313136d
jbmccormick <jonathan.b.mccormick@gmail.com>
2022-07-13 14:58:16 -0500

    Update MEC2Buttons.user.js

--- MEC2Buttons.user.js
+++ MEC2Buttons.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Add navigation buttons to MEC2 to replace the drop down hover menus
 // @author       MECH2
-// @match        mec2.prod2.dhs.state.mn.us/*
+// @match        mec2.childcare.dhs.state.mn.us*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/jbmccormick/MEC2Web/master/MEC2Buttons.user.js
 // ==/UserScript==
